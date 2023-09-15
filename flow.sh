bc6fd654d47e757f9f444b6f3d6ec7f6f2798353
lakshminandini <89056690+sririshi120@users.noreply.github.com>
2023-09-15 18:14:34 +0530

    Update flow.sh

--- flow.sh
+++ flow.sh
@@ -1,25 +1,34 @@
-
-
-what is the output 
-TERRAGRUNT_CMD+="$TG_WORKDIR"
-echo "$TERRAGRUNT_CMD"
-
-
 #!/bin/bash
 
-# Check if this is a pull request
-if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
-  # Check if the pull request is merged
-  if [ "$GITHUB_EVENT_ACTION" == "closed" ] && [ "$GITHUB_EVENT_PULL_REQUEST_MERGED" == "true" ]; then
-    echo "Pull request was merged. Proceeding to apply Terraform changes..."
-    # Run Terraform apply here
-    terraform apply -auto-approve
-  else
-    echo "Pull request was not merged. Terraform apply is not required."
-  fi
+# Define the target branch to compare with
+target_branch="main"  # Replace with your desired branch name
+
+# Function to get a list of changed files
+get_changed_files() {
+  git diff --name-only "$target_branch"
+}
+
+# Check if the target branch has been merged
+if git branch --list --remote "origin/$target_branch" | grep "$target_branch" &> /dev/null; then
+  echo "The $target_branch branch has been merged. Proceeding with Terraform apply..."
+
+  # Run Terraform apply here
+  terraform apply -auto-approve
+
+  # Get a list of changed files
+  changed_files=$(get_changed_files)
+
+  # Count and print the number of changed files
+  file_count=$(echo "$changed_files" | wc -l)
+  echo "Number of changed files: $file_count"
+
+  # Print the list of changed files
+  echo "List of changed files:"
+  echo "$changed_files"
 else
-  echo "This is not a pull request. Terraform apply is not required."
+  echo "The $target_branch branch has not been merged. Terraform apply is not required."
 fi
+
 
 ===================================
 
